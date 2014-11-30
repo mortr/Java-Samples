5c19af8dcd78653b97cb53b5d750146facea4c96
Kirill Rozov <kirich1409@gmail.com>
2014-11-30 13:11:39 +0300

    Update Java basics sample.

--- Generics/src/by/htp/krozov/sample/generics/Main.java
+++ Generics/src/by/htp/krozov/sample/generics/Main.java
@@ -24,6 +24,8 @@ public class Main {
 
     public static void main(String[] args) {
         Pair<String, Integer> pair = new Pair<String, Integer>("key", 1);
-        Pair<? super Main, Serializable> pair2 = new Pair<Main, Serializable>("key", 1);
+
+        Pair<? extends CharSequence, Serializable> pair2 = new Pair<String, Serializable>("key", 1);
+        Pair<? super String, Serializable> pair3 = new Pair<CharSequence, Serializable>("key", 1);
     }
 }
