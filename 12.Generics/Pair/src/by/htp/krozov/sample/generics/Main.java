4c699cd37041bb6a75345147ade1ddc36962358b
Kirill Rozov <kirich1409@gmail.com>
2014-12-16 20:33:57 +0300

    Add Generic Pair sample.

--- 12.Generics/Pair/src/by/htp/krozov/sample/generics/Main.java
+++ 12.Generics/Pair/src/by/htp/krozov/sample/generics/Main.java
@@ -4,24 +4,6 @@ import java.io.Serializable;
 
 public class Main {
 
-    public static class Pair<K, V> {
-        private K key;
-        private V value;
-
-        public Pair(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-
-        public K getKey() {
-            return key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-    }
-
     public static void main(String[] args) {
         Pair<String, Integer> pair = new Pair<String, Integer>("key", 1);
 
---
+++ 12.Generics/Pair/src/by/htp/krozov/sample/generics/Pair.java
@@ -0,0 +1,22 @@
+package by.htp.krozov.sample.generics;
+
+/**
+* Created by krozov on 16.12.14.
+*/
+public class Pair<K, V> {
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
